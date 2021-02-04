d63561ec4d90a41193408f51d21232679c976f7b
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2021-02-04 22:14:27 +0100

    Step 2 - Generalize case for multiple numbers

--- src/main/java/my_package/StringCalculator.java
+++ src/main/java/my_package/StringCalculator.java
@@ -1,14 +1,12 @@
 package my_package;
 
-import static java.lang.Integer.parseInt;
+import static java.util.Arrays.stream;
 
 public class StringCalculator {
     public static int add(String input) {
         if (input.isEmpty()) return 0;
 
         String[] numbers = input.split(",");
-        if (numbers.length == 1) return parseInt(numbers[0]);
-        else if (numbers.length == 2) return parseInt(numbers[0]) + parseInt(numbers[1]);
-        return 6;
+        return stream(numbers).mapToInt(Integer::parseInt).sum();
     }
 }
