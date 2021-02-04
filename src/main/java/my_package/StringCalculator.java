c98b89003fdb83f23165e1977851b0d0f67e9473
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2021-02-04 21:57:30 +0100

    Step - 1 Generalize case for two numbers on string

--- src/main/java/my_package/StringCalculator.java
+++ src/main/java/my_package/StringCalculator.java
@@ -1,11 +1,13 @@
 package my_package;
 
+import static java.lang.Integer.parseInt;
+
 public class StringCalculator {
     public static int add(String input) {
         if (input.isEmpty()) return 0;
 
         String[] numbers = input.split(",");
-        if (numbers.length == 1) return Integer.parseInt(numbers[0]);
-        else return 3;
+        if (numbers.length == 1) return parseInt(numbers[0]);
+        return parseInt(numbers[0]) + parseInt(numbers[1]);
     }
 }
--- src/test/java/my_package/StringCalculatorTest.java
+++ src/test/java/my_package/StringCalculatorTest.java
@@ -3,11 +3,18 @@ package my_package;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.stream.Stream;
+
 import static java.lang.Integer.parseInt;
+import static java.util.stream.Stream.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class StringCalculatorTest {
 
@@ -27,10 +34,19 @@ public class StringCalculatorTest {
         assertThat(result, is(numberAsInteger));
     }
 
-    @Test
-    public void when_string_two_numbers_result_is_the_of_them() {
-        int result = StringCalculator.add("1,2");
+    private static Stream<Arguments> numbersAndItsSum() {
+        return of(
+                arguments("1,2", 3),
+                arguments("10,21", 31),
+                arguments("0,23", 23)
+        );
+    }
 
-        assertThat(result, is(3));
+    @ParameterizedTest
+    @MethodSource("numbersAndItsSum")
+    public void when_string_two_numbers_result_is_the_of_them(String input, int addedNumbers) {
+        int result = StringCalculator.add(input);
+
+        assertThat(result, is(addedNumbers));
     }
 }
