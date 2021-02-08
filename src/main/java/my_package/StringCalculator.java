6e57bf6d8d4a03066a07d6131459f660757cb6c8
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2021-02-08 13:03:32 +0100

    Step 3 - Extract input splitting into function

--- src/main/java/my_package/StringCalculator.java
+++ src/main/java/my_package/StringCalculator.java
@@ -1,12 +1,19 @@
 package my_package;
 
+import java.util.stream.Stream;
+
 import static java.util.Arrays.stream;
 
 public class StringCalculator {
     public static int add(String input) {
         if (input.isEmpty()) return 0;
 
-        String[] numbers = input.split(",");
-        return stream(numbers).flatMap(x -> stream(x.split("\n"))).mapToInt(Integer::parseInt).sum();
+        Stream<String> numbers = splitIntoNumbers(input);
+        return numbers.mapToInt(Integer::parseInt).sum();
+    }
+
+    private static Stream<String> splitIntoNumbers(String input) {
+        return Stream.of(input.split(","))
+                .flatMap(x -> stream(x.split("\n")));
     }
 }
