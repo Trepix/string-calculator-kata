934a38745eaabdda38ab6f6cfe8c3f5cba4692c1
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2021-02-09 00:31:52 +0100

    Step 4 - Generalize code for delimiters

--- src/main/java/my_package/StringCalculator.java
+++ src/main/java/my_package/StringCalculator.java
@@ -13,7 +13,11 @@ public class StringCalculator {
     }
 
     private static Stream<String> splitIntoNumbers(String input) {
-        if (input.startsWith("//")) return Stream.of("3");
+        if (input.startsWith("//")) {
+            String delimiter = input.split("\n")[0].replace("//", "");
+            String numbers = input.split("\n")[1];
+            return Stream.of(numbers.split(delimiter));
+        }
         return Stream.of(input.split(","))
                 .flatMap(x -> stream(x.split("\n")));
     }
