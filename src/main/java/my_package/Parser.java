c1c10f3dd2befdef836ce3e081ce2a242c0941b0
Trepix <pau.trepat.segura@gmail.com>
2021-03-20 03:29:48 +0100

    Step 10 - Simplify solution
    Because problem only requires to check valid inputs, after all, what we need is a group of integers separated by some kind of characters, so the regex could be simpler than escaped characters for use it in regex, etc.

--- src/main/java/my_package/Parser.java
+++ src/main/java/my_package/Parser.java
@@ -1,48 +1,22 @@
 package my_package;
 
 
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.Arrays.asList;
 
 public class Parser {
 
-    public static final String DEFAULT_DELIMITERS = "[,|\n]";
-    public static final String CUSTOM_DELIMITER_SEQUENCE = "//";
-    public static final List<Character> CHARACTERS_TO_ESCAPE_IN_REGEX = asList('.', '^', '$', '*', '+', '?', '(', ')', '[', '{', '\\', '|');
-
     static IntStream splitIntoNumbers(String input) {
-        return splitByDelimiter(input).mapToInt(Integer::parseInt);
-    }
-
-    private static Stream<String> splitByDelimiter(String input) {
-        String delimiter = DEFAULT_DELIMITERS;
-        String numbers = input;
-        if (input.startsWith(CUSTOM_DELIMITER_SEQUENCE)) {
-            String[] inputSplitByEndLines = input.split("\n");
-            delimiter = getDelimiter(inputSplitByEndLines[0]);
-            numbers = inputSplitByEndLines[1];
+        Pattern pattern = Pattern.compile("([-\\d]+)");
+        Matcher matcher = pattern.matcher(input);
+        List<String> numbers = new LinkedList<>();
+        while (matcher.find()) {
+            numbers.add(matcher.group());
         }
-        return Stream.of(numbers.split(delimiter));
-    }
-
-    private static String getDelimiter(String inputSplitByEndLine) {
-        return inputSplitByEndLine
-                .replace(CUSTOM_DELIMITER_SEQUENCE, "")
-                .replace("[", "")
-                .replace("]", "")
-                .chars()
-                .mapToObj(i -> (char)i)
-                .map(Parser::escapeIfNecessary)
-                .collect(Collectors.joining());
-    }
-
-    private static String escapeIfNecessary(Character character) {
-        if (CHARACTERS_TO_ESCAPE_IN_REGEX.contains(character)) return "\\" + character;
-        else return character + "";
+        return numbers.stream().mapToInt(Integer::parseInt);
     }
 
 }
